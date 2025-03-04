8d7cfd5a86faac2bfe511a2cc9c0fb4681ba5671
nehalpanchall <nehalpanchal40@gmail.com>
2025-03-04 01:57:51 -0500

    feat: dragable attribute set true on task elements and set event dragstart on each tasks

--- kanban.js
+++ kanban.js
@@ -2,6 +2,8 @@ const addTaskbtn = document.querySelector('#addTask');
 
 const board = document.querySelector('.board');
 
+const tasks = document.querySelectorAll('.task');
+
 addTaskbtn.addEventListener('click', () => {
   const paragraphTag = document.createElement('p');
   paragraphTag.innerText = prompt('Task name');
@@ -9,4 +11,12 @@ addTaskbtn.addEventListener('click', () => {
   paragraphTag.classList.add('task');
 
   board.appendChild(paragraphTag);
+
+  paragraphTag.setAttribute('draggable', true);
+});
+
+tasks.forEach((task) => {
+  return task.addEventListener('dragstart', () => {
+    console.log(`dragstart on`);
+  });
 });
